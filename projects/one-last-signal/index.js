1113d4117545d5b032105f294971615cdf728960
Ross <rossthurg@gmail.com>
2026-05-20 09:11:35 +0100

    Update import paths to use CDN links

--- projects/one-last-signal/index.js
+++ projects/one-last-signal/index.js
@@ -1,7 +1,7 @@
-import * as THREE from 'three';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger.js';
-import { GLTFLoader } from '/three/addons/loaders/GLTFLoader.js';
+import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+import gsap from 'https://cdn.jsdelivr.net/npm/gsap@3.12.5/index.js';
+import { ScrollTrigger } from 'https://cdn.jsdelivr.net/npm/gsap@3.12.5/ScrollTrigger.js'
+import { GLTFLoader } from 'https://unpkg.com/three/examples/jsm/loaders/GLTFLoader.js';
 
 gsap.registerPlugin(ScrollTrigger);
 
